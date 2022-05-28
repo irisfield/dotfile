1f887a4e4a667bbbda8906e70b966d89f87ae244
Alexander Quezada <alexander.quezada@lc.cuny.edu>
2022-05-28 15:23:54 -0400

    updated preferences

--- .config/nvim/lua/iris/lsp/null-ls.lua
+++ .config/nvim/lua/iris/lsp/null-ls.lua
@@ -13,10 +13,10 @@ local indentwidth = vim.api.nvim_buf_get_option(0, "shiftwidth")
 null_ls.setup({
   debug = false,
   sources = {
-    formatting.black,
+    formatting.black.with({ extra_args = { "--line-length", "120" } }),
     formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
     formatting.stylua.with({ extra_args = { "--indent-type", "spaces", "--indent-width", indentwidth } }),
-    diagnostics.flake8,
+    diagnostics.flake8.with({ extra_args = { "--max-line-length", "120", "--ignore", "W391,E302,E301" } }), -- https://pycodestyle.pycqa.org/en/latest/intro.html#error-codes
   },
   -- you can reuse a shared lspconfig on_attach callback here
   on_attach = function(client)
